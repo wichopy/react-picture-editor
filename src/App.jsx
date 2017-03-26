719cc416f649710b71752883c7f4cafe53c8dd6d
william chou <w.chou06@gmail.com>
2017-03-26 10:34:19 -0400

    Successfully have sliders working.

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 import './App.css';
 const meme = require('../public/all-the-things-blank-clean-template-cropped.png')
 import { Button, Row, Col, Icon } from 'react-materialize';
-var Dropzone = require('react-dropzone');
+const Dropzone = require('react-dropzone');
 
 
-var DropzoneDemo = React.createClass({
+const DropzoneDemo = React.createClass({
     getInitialState: function () {
         return {
           files: []
@@ -29,7 +29,11 @@ var DropzoneDemo = React.createClass({
             this.state.files.length > 0 
             ? //IF TRUE
             <div>
-              <div>{this.state.files.map((file) => <img src={file.preview} /> )}</div>
+              <div>
+                {this.state.files.map((file) => 
+                  <img className="img" src={file.preview} style={this.props.styles}/> 
+                )}
+              </div>
             </div> 
             : //IF FALSE
             <div>
@@ -53,7 +57,7 @@ class App extends Component {
       rotation: 0,
       opacity: 100,
       scale: 100,
-      translate: 0
+      translate: 0,
     };
 
     this.adjustRotation = this.adjustRotation.bind(this);
@@ -87,30 +91,44 @@ class App extends Component {
   adjustTranslate (event) {
     this.setState({translate: event.target.value });
   }
+
   render() {
+
     return (
       <div className="App">
         <div className="App-header">
-          <span>
           <img src={meme} className="meme"/>
-          <h2>Play with all the pictures</h2>
-          </span>
+          <h2 className="title">Play with all the pictures</h2>
         </div>
         <Row className="App-main-container card-panel grey lighten-2">
           <Col s={9} className='grid-example'>
           <div className="card-panel white picture-wrapper valign-wrapper center-align">          
-            <DropzoneDemo className="valign center-align" />
+            <DropzoneDemo className="valign center-align" 
+              styles={
+                {
+                  opacity: this.state.opacity/100,
+                  transform: `rotate(${this.state.rotation}deg) translateX(${this.state.translate}px)`,
+                  height: `${this.state.scale}%`,
+                  width: `${this.state.scale}%`
+                }
+              } />
           </div>
           </Col>
           <Col s={3} className='avail-buttons'>
           <p>Rotate</p>
-          <p><input className="valign" type="range" min="0" max="360" value={this.state.rotation} step="1" onChange={this.adjustRotation}/>{this.state.rotation}</p>
+            <div class="chip">
+              0 deg
+            </div>
+             <div class="chip">
+              360 deg
+            </div>
+          <p className="range-field"><input className="valign" type="range" min="0" max="360" value={this.state.rotation} step="1" onChange={this.adjustRotation}/></p>
           <p>Translate</p>
-          <p><input className="valign" type="range" min="-100" max="100" value={this.state.translate} step="1" onChange={this.adjustTranslate}/>{this.state.translate}</p>
+          <p className="range-field"><input className="valign" type="range" min="-100" max="100" value={this.state.translate} step="1" onChange={this.adjustTranslate}/></p>
           <p>Opacity</p>
-          <p><input className="valign" type="range" min="0" max="100" value={this.state.opacity} step="1" onChange={this.adjustOpactity}/>{this.state.opacity}</p>
+          <p className="range-field"><input className="valign" type="range" min="0" max="100" value={this.state.opacity} step="1" onChange={this.adjustOpactity}/></p>
           <p>Scale</p>
-          <p><input className="valign" type="range" min="0" max="100" value={this.state.scale} step="1" onChange={this.adjustScale}/>{this.state.scale}</p>
+          <p className="range-field"><input className="valign" type="range" min="0" max="100" value={this.state.scale} step="1" onChange={this.adjustScale}/></p>
           <p><Button onClick={this.defaultValues}>Reset</Button></p>
           </Col>
         </Row>
